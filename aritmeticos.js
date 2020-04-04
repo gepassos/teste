a46389fc4dfa14e2692805d1e87056d42d0a51d4
gepassos <gustavo.escuderopassos@gmail.com>
2020-04-04 17:53:11 -0400

    first

---
+++ aritmeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [4, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // resto da divisao divido por 2, se for numero redondo ele retorna 0 se for 1 ele é número com sobras.
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+
+ 
